dfc1ac01608c1b94415ce76066cd7f6b0d7847ed
centraldescout <naosediscutem@gmail.com>
2026-05-09 16:02:51 -0300

    Add multi club tenancy structure

---
+++ src/lib/tenancy/club-structure.ts
@@ -0,0 +1,4 @@
+export const scoutingDepartments=[{id:'dep-1',clubId:'guarani',name:'First Team Recruitment'}]
+export const squads=[{id:'sq-1',clubId:'guarani',name:'Senior Squad'}]
+export const watchlists=[{id:'wl-1',clubId:'guarani',name:'Shortlist',priority:'high'}]
+export const clubs=[{id:'guarani',name:'Guarani FC',style:'positional play'}]
